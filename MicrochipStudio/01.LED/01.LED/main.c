18232947e97b41a8b51390e6040b5495713c05a8
CoolDong <s_cooldong@naver.com>
2025-12-03 23:08:14 +0900

    Add LED Test

---
+++ MicrochipStudio/01.LED/01.LED/main.c
@@ -0,0 +1,24 @@
+/*
+ * 01.LED.c
+ *
+ * Created: 2025-12-03 오후 10:44:16
+ * Author : n_coo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRA = 0b11111111;
+	PORTA = 0b10101110;
+	
+    ///* Replace with your application code */
+    //while (1)
+    //{
+	    //
+    //}
+	
+	return 0;
+}
+
