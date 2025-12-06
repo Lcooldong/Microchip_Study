ee40f7d82eadf1dade6c8c99e57edea92fb5e731
CoolDong <s_cooldong@naver.com>
2025-12-06 19:54:33 +0900

    .

--- MicrochipStudio/01.LED/01.LED/main.c
+++ MicrochipStudio/01.LED/01.LED/main.c
@@ -5,19 +5,24 @@
  * Author : n_coo
  */ 
 
+#define F_CPU 16000000UL  // 16MHz , util/delay.h 보다 앞에 있어야함
 #include <avr/io.h>
+#include <util/delay.h>
+
 
 
 int main(void)
 {
-	DDRA = 0b11111111;
-	PORTA = 0b10101110;
 	
-    ///* Replace with your application code */
-    //while (1)
-    //{
-	    //
-    //}
+	DDRA = 0xFF; //DDRA = 0b11111111; , DDRA = 255
+	
+    while (1)
+    {
+		PORTA = 0b01010101;
+		_delay_ms(1000);
+		PORTA = 0b10101010;
+		_delay_ms(1000);
+    }
 	
 	return 0;
 }
